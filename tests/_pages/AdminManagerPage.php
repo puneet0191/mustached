bf9c9749dfec948b1a4e5bf73c70b68c2c90f650
Puneet Kala <puneet.0191@gmail.com>
2014-11-22 19:38:36 +0530

    Updating Header, removing hardcoded url's

--- tests/_pages/AdminManagerPage.php
+++ tests/_pages/AdminManagerPage.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @package     RedMEMBER2
+ * @package     mustached
  * @subpackage  Page Class
- * @copyright   Copyright (C) 2012 - 2014 redCOMPONENT.com. All rights reserved.
+ * @copyright   Copyright (C) 2014 mustached.org All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
@@ -16,28 +16,7 @@
 class AdminManagerPage
 {
     public static $allExtensionPages = array (
-        'Users Manager' => '/administrator/index.php?option=com_redmember&view=rmadminusers',
-        'Groups Manager' => '/administrator/index.php?option=com_redmember&view=rmadmingroups',
-        'Access Manager' => '/administrator/index.php?option=com_redmember&view=rmadminlevels',
-        'Organization Manager' => '/administrator/index.php?option=com_redmember&view=rmadminorganizations',
-        'Tabs Manager' => '/administrator/index.php?option=com_redmember&view=rmadmintabs',
-        'Membership Manager' => '/administrator/index.php?option=com_redmember&view=rmadminmemberships',
-        'Membership Orders Manager' => '/administrator/index.php?option=com_redmember&view=rmadminmembershiporders',
-        'Custom Fields Manager' => '/administrator/index.php?option=com_redmember&view=rmadminfields',
-        'Vat Groups Manager' => '/administrator/index.php?option=com_redmember&view=rmadminvatgroups',
-        'Vat Rates Manager' => '/administrator/index.php?option=com_redmember&view=rmadminvatrates',
-        'Templates Manager' => '/administrator/index.php?option=com_redmember&view=rmadmintemplates',
-        'Forum Section Manager' => '/administrator/index.php?option=com_redmember&view=rmadminforumsections',
-        'Forum Categories Manager' => '/administrator/index.php?option=com_redmember&view=rmadminforumcategories',
-        'Forum Threads Manager' => '/administrator/index.php?option=com_redmember&view=rmadminforumthreads',
-        'Forum Posts Manager' => '/administrator/index.php?option=com_redmember&view=rmadminforumposts',
-        'Folder Category Manager' => '/administrator/index.php?option=com_redmember&view=rmadminfoldercategories',
-        'Folder Manager' => '/administrator/index.php?option=com_redmember&view=rmadminfolders',
-        'Messages Manager' => '/administrator/index.php?option=com_redmember&view=rmadminmessages',
-        'Mail Templates Manager' => '/administrator/index.php?option=com_redmember&view=rmadminmails',
-        'News Letter Manager' => '/administrator/index.php?option=com_redmember&view=rmadminnewsletters',
-        'NewsLetter Subscriptions Manager' => '/administrator/index.php?option=com_redmember&view=rmadminnewslettersubscriptions',
-        'Search Forms Manager' => '/administrator/index.php?option=com_redmember&view=rmadminsearchforms',
-        'Search Manager' => '/administrator/index.php?option=com_redmember&view=rmadminsearch'
+        //put in URL's for all the pages of your extension, so that mustached can verify presence of notices and errors on them. ex.
+        //'Search Manager' => '/administrator/index.php?option=com_extensionname&view=adminsearch'
     );
 }
